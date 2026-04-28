f05c8ff391b09e348810c8f97a0446411104415f
Ratnaprava Mohapatra <144581192+RatnapravaMohapatra@users.noreply.github.com>
2026-04-28 11:59:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import os
 import uvicorn
 
-if __name__ == "__main__":
+
+def run():
+    env = os.getenv("ENV", "dev").lower()
+
+    is_dev = env == "dev"
+
     uvicorn.run(
         "api.main:app",
         host="0.0.0.0",
-        port=8000,
-        reload=True,
+        port=int(os.getenv("PORT", 8000)),
+        reload=is_dev,  # ✅ only in dev
+        workers=1 if is_dev else int(os.getenv("WORKERS", 2)),
         log_level="info",
     )
+
+
+if __name__ == "__main__":
+    run()
